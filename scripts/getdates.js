725aaf8e49301dfeedfb82704b019c936e71b16c
DiscipleDeveloper <ntumale@byupathway.edu>
2025-09-05 04:04:39 +0800

    Did simplier way

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,6 +3,5 @@ const today = new Date();
 const year = document.querySelector("#yearNow");
 year.innerHTML = today.getFullYear();
 
-let oLastModif = new Date(document.lastModified);
-let show = document.querySelector("#lastModified")
-show.textContent = oLastModif
+let show = document.getElementById("lastModified")
+show.textContent = document.lastModified
