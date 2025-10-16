2423371191532bcbdb4b03f5da6ae20ed119ed95
DiscipleDeveloper <ntumale@byupathway.edu>
2025-10-17 04:28:08 +0800

    finished some design for my website

---
+++ project/scripts/brightenglishspeaker.js
@@ -0,0 +1,16 @@
+
+// Making a hamburger button
+const menuButton = document.querySelector("#menu");
+const ul = document.querySelector("ul");
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open")
+    ul.classList.toggle("open");
+});
+
+const today = new Date();
+const year = document.querySelector("#yearNow");
+year.innerHTML = today.getFullYear();
+
+let show = document.getElementById("lastModified")
+show.textContent = document.lastModified
